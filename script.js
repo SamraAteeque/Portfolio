044866ebd3a64f3497dfd71dd8536c096cf6a7da
SamraAteeque <114831554+SamraAteeque@users.noreply.github.com>
2024-08-18 14:59:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const lightbox = document.getElementById('lightbox');
-//     const lightboxImg = document.getElementById('lightbox-img');
-//     const closeBtn = document.getElementsByClassName('close')[0];
-//     const certificateLinks = document.querySelectorAll('.certificate-link');
 
-//     certificateLinks.forEach(link => {
-//         link.addEventListener('click', function (e) {
-//             e.preventDefault();
-//             lightbox.style.display = 'block';
-//             lightboxImg.src = this.href;
-//         });
-//     });
-
-//     closeBtn.addEventListener('click', function () {
-//         lightbox.style.display = 'none';
-//     });
-
-//     lightbox.addEventListener('click', function (e) {
-//         if (e.target === lightbox) {
-//             lightbox.style.display = 'none';
-//         }
-//     });
-// });
-
-
-
-// const toggleBtn = document.querySelector('.toggle_btn');
-// const navLinks = document.querySelector('.nav-links');
-
-// toggleBtn.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-// });
 function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     const navMobOpen = document.querySelector('.nav-mob-open');
